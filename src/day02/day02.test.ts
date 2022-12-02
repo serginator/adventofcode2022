8484d9c08d9e49eae6798b13b30f4da9897c7996
Sergio Ruiz <serginator@gmail.com>
2022-12-02 10:32:33 +0100

    day02 - test 100% coverage

--- src/day02/day02.test.ts
+++ src/day02/day02.test.ts
@@ -14,4 +14,22 @@ describe('Day 02 - Rock Paper Scissors', () => {
     const expected = 12;
     expect(day02.part02(input)).toBe(expected);
   });
+  const inputAllCases = `A X
+      B Y
+      C Z
+      A Y
+      B X
+      C X
+      A Z
+      B Z
+      C Y
+    `;
+  it ('test all options part1', () => {
+    const expected = 45;
+    expect(day02.part01(inputAllCases)).toBe(expected);
+  });
+  it ('test all options part2', () => {
+    const expected = 45;
+    expect(day02.part02(inputAllCases)).toBe(expected);
+  });
 })
